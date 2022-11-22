45d6db5af62cec930cfcc8f42b8c4214e303422e
Nikita Polozov <npolozov0@gmail.com>
2022-11-22 10:05:26 +0200

    1

--- src/components/Statistic/title_two/statsTitle.jsx
+++ src/components/Statistic/title_two/statsTitle.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { Title } from './statsTitle.styled';
 
 export const StatsTitle = ({ text }) => {
-  return <div>{text && <Title>{text}</Title>}</div>;
+  return text && <Title>{text}</Title>;
 };
 
 StatsTitle.propTypes = {
